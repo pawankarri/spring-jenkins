44ab71640ec6f0ed09bf0d6d1e4d16bd6466424c
pawankarri <121856461+pawankarri@users.noreply.github.com>
2023-09-06 15:37:35 +0530

    Update SpringJenkinsApplication

--- src/main/java/com/jenkins/SpringJenkinsApplication.java
+++ src/main/java/com/jenkins/SpringJenkinsApplication.java
@@ -4,13 +4,20 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @Slf4j
-public class SpringJenkinsApplication {
+public class SpringJenkinsApplication extends SpringBootServletInitializer {
 @PostConstruct
 	public void intt(){
 		log.info("application started..........");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringJenkinsApplication.class);
 	}
 
 	public static void main(String[] args) {
